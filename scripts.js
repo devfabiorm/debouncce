fcc68cd5a842df9defb7bb1438cebbc03a9cdc6f
Fabio Ribeiro Martins <5039966-fabiormdkp@users.noreply.gitlab.com>
2020-05-23 00:01:49 -0300

    Connecting to an API and delegating roles

--- scripts.js
+++ scripts.js
@@ -1,11 +1,29 @@
-let time = null;
+
+let users = [];
+const filterUsers = async name =>
+    fetch(`https://jsonplaceholder.typicode.com/users?name_like=${name}`)
+        .then(res => res.json());
+
+
+
+function debounceEvent() {
+
+    let time = null;
+
+    //closure
+    return function (value) {
+        clearTimeout(time);
+        time = setTimeout(() => {
+            filterUsers(value)
+                .then(users => console.log(users.map(user => user.name)));
+        }, 1000)
+    }
+}
+
+const debounce = debounceEvent();
+
 function handleKeyUp(event) {
-    
-    clearTimeout(time);
-        
-    time = setTimeout(() => {
-        console.log(event.target.value);
-    }, 1000)
+    debounce(event.target.value)
 }
 
 document.querySelector('input')
